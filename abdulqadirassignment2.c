dd997e7aebd33feda2e9e0b06618b132af654b53
Qadir9813 <93485606+Qadir9813@users.noreply.github.com>
2021-11-12 18:18:49 +0500

    Add files via upload

---
+++ abdulqadirassignment2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(void){
+
+  int pid, status;
+  pid = fork();
+  if(pid==0){
+  int num = 3; // Child Process
+   for(int i=1; i<=10; i++){
+     printf("%d x %d = %d\n",num, i, num*i);
+  }
+ }
+ else{ // Parent Process
+   wait(&status);
+   printf("In the parent Process :/nName is  Abdul Qadir CNIC 36402-8097333-3\n");
+ }
+
+ return 0;
+}
